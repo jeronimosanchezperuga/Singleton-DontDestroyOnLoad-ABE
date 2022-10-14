1a78b25511904b74cf4b03b67bfe0786fbbdf97c
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-10-14 10:46:26 -0300

    Initial commit

---
+++ Assets/Persistator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persistator : MonoBehaviour
+{
+    public static Persistator instance;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }    
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public SceneChanger sceneChangerScript;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sceneChangerScript.CambiarDeEscenaSecuencialmente();
+        }
+    }
+}
